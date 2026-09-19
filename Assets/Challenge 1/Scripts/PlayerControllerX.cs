e0685d6be79f2fad51539a5f873da9a063098931
AzafTechnologies <azafsathar0011@gmail.com>
2026-09-19 18:47:55 +0530

    Upgrade challenge scene and flight controls
    This change updates the Unity scene and project settings for the current editor, including URP/light settings and newer serialization metadata. It also reworks the plane controller to use the Input System with WASD-based movement, updates the camera follow offset to match the scene, and refreshes the black material configuration for compatibility.

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControllerX : MonoBehaviour
 {
@@ -8,22 +9,28 @@ public class PlayerControllerX : MonoBehaviour
     public float rotationSpeed;
     public float verticalInput;
 
+    public InputAction moveAction;
+
+    public Vector2 moveInput;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
+        moveInput = moveAction.ReadValue<Vector2>();
+       
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime );
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * moveInput.y);
     }
 }
